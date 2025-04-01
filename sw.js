03f743e7a9c804fc1d1e608c8db1ba3cfba48753
1M1ke <15212311846@163.com>
2025-04-01 09:35:24 +0800

    Site updated: 2025-04-01 09:35:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1743470316694";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1743471319029";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
